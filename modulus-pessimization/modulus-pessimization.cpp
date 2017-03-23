31b972b2a56ce3093bc7728bbc692bb291b63524
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-03-23 17:11:41 -0400

    Bug: non-static const/constexpr is not strength reduced within a lambda

--- modulus-pessimization/modulus-pessimization.cpp
+++ modulus-pessimization/modulus-pessimization.cpp
@@ -1,11 +1,27 @@
-// modulus-pessimization.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 
+volatile std::atomic<size_t> global = 0;
 
 int main()
 {
-    return 0;
-}
+	static const     size_t  sc_count = 2ULL;
+	       constexpr size_t  ce_count = 2ULL;
+	static constexpr size_t sce_count = 2ULL;
+	       const     size_t   c_count = 2ULL;
+	
+	size_t  sc_result_direct =              global.load(std::memory_order_acquire) %  sc_count; // and reg, 1
+	size_t  ce_result_direct =              global.load(std::memory_order_acquire) %  ce_count; // and reg, 1
+	size_t sce_result_direct =              global.load(std::memory_order_acquire) % sce_count; // and reg, 1
+	size_t   c_result_direct =              global.load(std::memory_order_acquire) %   c_count; // and reg, 1
 
+	size_t  sc_result_lambda = [&] { return global.load(std::memory_order_acquire) %  sc_count; }(); // and reg, 1
+	size_t  ce_result_lambda = [&] { return global.load(std::memory_order_acquire) %  ce_count; }(); // div
+	size_t sce_result_lambda = [&] { return global.load(std::memory_order_acquire) % sce_count; }(); // and reg, 1
+	size_t   c_result_lambda = [&] { return global.load(std::memory_order_acquire) %   c_count; }(); // div
+
+
+	std::cout << sc_result_direct << " " << ce_result_direct << " " << sce_result_direct << " " << c_result_direct << std::endl;
+	std::cout << sc_result_lambda << " " << ce_result_lambda << " " << sce_result_lambda << " " << c_result_lambda << std::endl;
+
+	return 0;
+}
--- modulus-pessimization/stdafx.h
+++ modulus-pessimization/stdafx.h
@@ -1,15 +1,15 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #include "targetver.h"
 
-#include <stdio.h>
-#include <tchar.h>
+#pragma warning(disable: 4514) // warning C4514: '%s': unreferenced inline function has been removed
+#pragma warning(disable: 4710) // warning C4710: '%s': function not inlined
+#pragma warning(disable: 4668) // warning C4668: '%s' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
+#pragma warning(disable: 4820) // warning C4820: '%s': '%d' bytes padding added after data member '%s'
 
+#if !defined(_STL_EXTRA_DISABLED_WARNINGS)
+#define _STL_EXTRA_DISABLED_WARNINGS 4061 4365 4514 4571 4623 4625 4626 4710 4774 4820 4987 5026 5027
+#endif
 
-
-// TODO: reference additional headers your program requires here
+#include <atomic>
+#include <iostream>
