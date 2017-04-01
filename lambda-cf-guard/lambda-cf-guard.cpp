0083df5cb7078e45c37bc94cd022a83a9d043a26
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-04-01 14:07:12 -0400

    Include instructions for incremental build.

--- lambda-cf-guard/lambda-cf-guard.cpp
+++ lambda-cf-guard/lambda-cf-guard.cpp
@@ -6,6 +6,13 @@ int main()
 	using namespace std::chrono_literals;
 
 	std::vector<std::thread> threads;
+	// do a clean build, then comment out this for loop, and do an incremental build/link
+	for(size_t i = 0; i < 2; ++i) {
+		threads.push_back(std::thread([]() {
+			std::this_thread::sleep_for(1s);
+		}));
+	}
+
 	for(size_t i = 0; i < 2; ++i) {
 		threads.push_back(std::thread([](size_t num) {
 			std::this_thread::sleep_for(num * 1s);
@@ -17,4 +24,3 @@ int main()
 	std::cout << "not reached" << std::endl;
 	return 0;
 }
-
